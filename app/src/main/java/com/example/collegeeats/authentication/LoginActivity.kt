8e5ea043936e5127881e1be2079a8638c423def2
praveen kumar <94559585+prvn2004@users.noreply.github.com>
2023-01-08 22:13:07 +0530

    Added files for authentication

--- app/src/main/java/com/example/collegeeats/authentication/LoginActivity.kt
+++ app/src/main/java/com/example/collegeeats/authentication/LoginActivity.kt
@@ -1,11 +1,12 @@
-package com.example.collegeeats
+package com.example.collegeeats.authentication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.collegeeats.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }

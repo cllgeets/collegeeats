5568729a8cde92271c680533cbb9397ca4479ace
praveen kumar <94559585+prvn2004@users.noreply.github.com>
2023-01-12 19:25:25 +0530

    created login screen design in android studio

--- app/src/main/java/com/example/collegeeats/authentication/loginActivity.kt
+++ app/src/main/java/com/example/collegeeats/authentication/loginActivity.kt
@@ -2,11 +2,20 @@ package com.example.collegeeats.authentication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.collegeeats.R
 
 class loginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        showFragment(signupFragment())
+    }
+
+    fun showFragment(fragment: Fragment) {
+        val fram = supportFragmentManager.beginTransaction()
+        fram.replace(R.id.container, fragment)
+        fram.commit()
     }
 }
